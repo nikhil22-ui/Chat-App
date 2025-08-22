3b14c3563d7fc28c3d68a98a51239f074f760486
Nikhil  Kumar Suman <nikhilkumarsuman12345@gmail.com>
2025-08-23 00:48:12 +0530

    adding file public and server

--- public/chat-app/src/pages/Register.jsx
+++ public/chat-app/src/pages/Register.jsx
@@ -1,7 +1,57 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg";
 function Register(){
-  return <div>Register</div>
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("form");
+    };
+    const handleChange = (event) => {
+
+    }
+  return (
+    <>
+      <FormContainer>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+    </>
+  );
 }
+
+const FormContainer = styled.div``;
 
 export default Register;
