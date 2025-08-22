cb69ad55774a4719cc1da8d4bb1e672ce88621ee
Nikhil  Kumar Suman <nikhilkumarsuman12345@gmail.com>
2025-08-22 23:33:40 +0530

    adding files public and server

---
+++ public/chat-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
